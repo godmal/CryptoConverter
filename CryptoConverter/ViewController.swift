6f5d807295648624037901759134a8550c7e89ec
Dima Gorbachev <dimagorbachev@Dimas-Mac.local>
2017-07-17 10:00:23 -0700

    first commit

---
+++ CryptoConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoConverter
+//
+//  Created by Dima Gorbachev on 17.07.17.
+//  Copyright © 2017 Dima Gorbachev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
